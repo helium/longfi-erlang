1c0426af5b0ec7c6923f61659db7518fdd926828
Jay Kickliter <jay@kickliter.com>
2019-12-11 14:58:31 -0800

    repo: add clang-format specification
    Let's get this out of the way. There is a 0% change that I will
    manage to ignore muscle memory and not run clang-format before
    saving c sources.

--- c_src/longfi.c
+++ c_src/longfi.c
@@ -15,8 +15,9 @@
  */
 
 #include "erl_nif.h"
-#include "lfc/lfc.h"
 #include "lfc/fingerprint.h"
+#include "lfc/lfc.h"
+#include "lfc/priv/lfc_dg_des.h"
 #include <stdbool.h>
 #include <string.h>
 
@@ -24,46 +25,49 @@ static ERL_NIF_TERM ATOM_TRUE;
 static ERL_NIF_TERM ATOM_FALSE;
 
 static ERL_NIF_TERM
-erl_lfc_fingerprint_monolithic(ErlNifEnv * env, int argc, const ERL_NIF_TERM argv[]) {
-
+erl_lfc_fingerprint_monolithic(ErlNifEnv *        env,
+                               int                argc,
+                               const ERL_NIF_TERM argv[]) {
     ErlNifBinary key, payload;
-    int tmpint;
+    int          tmpint;
 
-    uint16_t        hdr;
-    uint32_t        oui;
-    uint32_t        did;
-    uint32_t        seq;
-    uint32_t        fp;
+    uint16_t hdr;
+    uint32_t oui;
+    uint32_t did;
+    uint32_t seq;
+    uint32_t fp;
 
-    if (! enif_inspect_binary(env, argv[0], &key)) {
+    if (!enif_inspect_binary(env, argv[0], &key)) {
         return enif_make_badarg(env);
     }
 
-    if (! enif_get_int(env, argv[1], &tmpint)) {
+    if (!enif_get_int(env, argv[1], &tmpint)) {
         return enif_make_badarg(env);
     }
     hdr = (uint16_t)tmpint;
 
-    if (! enif_get_int(env, argv[2], &tmpint)) {
+    if (!enif_get_int(env, argv[2], &tmpint)) {
         return enif_make_badarg(env);
     }
     oui = (uint32_t)tmpint;
 
-    if (! enif_get_int(env, argv[3], &tmpint)) {
+    if (!enif_get_int(env, argv[3], &tmpint)) {
         return enif_make_badarg(env);
     }
     did = (uint32_t)tmpint;
 
-    if (! enif_get_int(env, argv[4], &tmpint)) {
+    if (!enif_get_int(env, argv[4], &tmpint)) {
         return enif_make_badarg(env);
     }
     seq = (uint32_t)tmpint;
 
-    if (! enif_inspect_binary(env, argv[5], &payload)) {
+    if (!enif_inspect_binary(env, argv[5], &payload)) {
         return enif_make_badarg(env);
     }
 
-    if (lfc_fingerprint_monolithic(key.data, key.size, hdr, oui, did, seq, payload.data, payload.size, &fp) == lfc_res_ok) {
+    if (lfc_fingerprint_monolithic(
+            key.data, key.size, hdr, oui, did, seq, payload.data, payload.size, &fp)
+        == lfc_res_ok) {
         return enif_make_uint(env, fp);
     }
     return enif_make_badarg(env);
@@ -71,18 +75,13 @@ erl_lfc_fingerprint_monolithic(ErlNifEnv * env, int argc, const ERL_NIF_TERM arg
 
 
 #define ATOM(Id, Value)                                                        \
-    {                                                                          \
-        Id = enif_make_atom(env, Value);                                       \
-    }
+    { Id = enif_make_atom(env, Value); }
 
-static ErlNifFunc nif_funcs[] =
-{
-     {"fingerprint_monolithic", 6, erl_lfc_fingerprint_monolithic, 0}
-};
+static ErlNifFunc nif_funcs[] = {
+    {"fingerprint_monolithic", 6, erl_lfc_fingerprint_monolithic, 0}};
 
 static int
-load(ErlNifEnv * env, void ** priv_data, ERL_NIF_TERM load_info)
-{
+load(ErlNifEnv * env, void ** priv_data, ERL_NIF_TERM load_info) {
     (void)priv_data;
     (void)load_info;
 
