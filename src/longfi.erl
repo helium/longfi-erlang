a73160df26253e9a9f03fe8f976f8c9a14c3b725
Jay Kickliter <jay@kickliter.com>
2020-01-13 13:55:38 -0800

    Add types to record fields

--- src/longfi.erl
+++ src/longfi.erl
@@ -1,81 +1,95 @@
 -module(longfi).
 
--export([get_fingerprint/2]).
--export([serialize/5]).
--export([deserialize/1]).
+-export([deserialize/1,
+         get_fingerprint/2,
+         serialize/5]).
 
 -on_load(init/0).
 
--record(monolithic_flags, {downlink,
-                           should_ack,
-                           cts_rts,
-                           priority,
-                           ldpc}).
-
--record(monolithic, {flags,
-                     oui,
-                     did,
-                     fp,
-                     seq,
-                     payload}).
-
--record(ack_flags, {failure,
-                    session_expired,
-                    cts_rts,
-                    retransmit,
-                    ldpc}).
-
--record(ack, {flags,
-              oui,
-              did,
-              fp,
-              seq,
-              payload}).
-
--record(frame_start_flags, {downlink,
-                            should_ack,
-                            cts_rts,
-                            priority}).
-
--record(frame_start, {flags,
-                      oui,
-                      did,
-                      fp,
-                      seq,
-                      fragments,
-                      payload}).
-
--record(frame_data_flags, { ldpc }).
-
--record(frame_data, {flags,
-                     oui,
-                     did,
-                     fp,
-                     fragment,
-                     payload}).
-
 -include_lib("helium_proto/src/pb/helium_longfi_pb.hrl").
 
--define(APPNAME, longfi).
--define(LIBNAME, 'longfi').
+
+-record(monolithic_flags, {downlink :: boolean(),
+                           should_ack :: boolean(),
+                           cts_rts :: boolean(),
+                           priority :: boolean(),
+                           ldpc :: boolean()}).
+
+-record(monolithic, {flags :: #monolithic_flags{},
+                     oui :: non_neg_integer(),
+                     did :: non_neg_integer(),
+                     fp :: non_neg_integer(),
+                     seq :: non_neg_integer(),
+                     payload :: binary()}).
+
+
+-record(ack_flags, {failure :: boolean(),
+                    session_expired :: boolean(),
+                    cts_rts :: boolean(),
+                    retransmit :: boolean(),
+                    ldpc :: boolean()}).
+
+-record(ack, {flags :: #ack_flags{},
+              oui :: non_neg_integer(),
+              did :: non_neg_integer(),
+              fp :: non_neg_integer(),
+              seq :: non_neg_integer(),
+              payload :: binary()}).
+
+
+-record(frame_start_flags, {downlink :: boolean(),
+                            should_ack :: boolean(),
+                            cts_rts :: boolean(),
+                            priority :: boolean()}).
+
+-record(frame_start, {flags :: #frame_start_flags{},
+                      oui :: non_neg_integer(),
+                      did :: non_neg_integer(),
+                      fp :: non_neg_integer(),
+                      seq :: non_neg_integer(),
+                      fragments :: non_neg_integer(),
+                      payload :: binary()}).
+
+
+-record(frame_data_flags, { ldpc :: boolean() }).
+
+-record(frame_data, {flags :: #frame_data_flags{},
+                     oui :: non_neg_integer(),
+                     did :: non_neg_integer(),
+                     fp :: non_neg_integer(),
+                     fragment :: non_neg_integer(),
+                     payload :: binary()}).
+
+
+
 
 -spec get_fingerprint(#helium_LongFiRxPacket_pb{}, binary()) -> non_neg_integer().
 get_fingerprint(#helium_LongFiRxPacket_pb{tag_bits=Header, oui=OUI, device_id=DID, sequence=Sequence, payload=Payload}, Key) ->
     fingerprint_monolithic(Key, Header, OUI, DID, Sequence, Payload).
 
+
 fingerprint_monolithic(_Key, _Header, _OUI, _DID, _Sequence, _Payload) ->
     not_loaded(?LINE).
+
 
 -spec serialize(non_neg_integer(), non_neg_integer(), non_neg_integer(), non_neg_integer(), binary()) ->binary().
 serialize(OUI, DID, Sequence, Fingerprint, Payload) ->
     serialize_monolithic(OUI, DID, Sequence, Fingerprint, Payload).
 
+
 serialize_monolithic(_OUI, _DID, _Sequence, _Fingerprint, _Payload) ->
     not_loaded(?LINE).
+
 
 -spec deserialize(binary()) -> error | {ok, #monolithic{} | #ack{} | #frame_start{} | #frame_data{}}.
 deserialize(_Bin) ->
     not_loaded(?LINE).
+
+
+
+
+-define(APPNAME, longfi).
+-define(LIBNAME, 'longfi').
 
 init() ->
     SoName = case code:priv_dir(?APPNAME) of
