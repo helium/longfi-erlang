185634baedc1a018112bf597c93612a8581b8fce
Jeremy Cooper <jeremy.gthb@baymoo.org>
2021-02-26 01:55:22 +0000

    Remove unsual unintended CFLAGS and LDFLAGS additions.

--- c_src/compile.sh
+++ c_src/compile.sh
@@ -19,9 +19,6 @@ if [ ! "$VERSION" = "$CURRENT_VERSION" ]; then
     git checkout $VERSION
 fi
 
-export CFLAGS=-fPIC
-export LDFLAGS=-fPIC
-
 if [ ! -d build ]; then
     cmake -H. -Bbuild -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
 fi
